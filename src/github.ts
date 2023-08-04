15c4a9e09c01f04dd255106220cea2fb44c55327
Aristides Perez <aris.a.perez@gmail.com>
2023-08-04 09:51:39 -0400

    use dotenv for secrets

--- src/github.ts
+++ src/github.ts
@@ -1,5 +1,4 @@
-import { Octokit, App } from "octokit";
-import { createAppAuth } from "@octokit/auth-app";
+import { Octokit } from "octokit";
 
 // Octokit
 
@@ -13,13 +12,16 @@ const BLOG_PATH = "blog";
 const BLOG_INDEX_PATH = `${BLOG_PATH}/index.html`;
 
 export async function octo({ path, title, content }: { path: string, title: string, content: string }) {
+  // NOTE: Disabling linting errors on third party libraries (octokit)
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
   const octokit = new Octokit({
-    auth: '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'
+    auth: process.env.API_KEY
   })
 
   debugger  // eslint-disable-line no-debugger
 
   try {
+    // NOTE: Disabling linting errors on third party libraries (octokit)
     const response = await octokit.request(`PUT /repos/${OWNER}/${REPO}/contents/${path}`, {
       owner: OWNER,
       repo: REPO,
@@ -31,15 +33,16 @@ export async function octo({ path, title, content }: { path: string, title: stri
         'X-GitHub-Api-Version': '2022-11-28'
       }
     })
+    /* eslint-enable */
 
     if (response.ok) {
       debugger  // eslint-disable-line no-debugger
 
     } else {
-
+      console.log("BAR")
     }
 
   } catch (error) {
-    debugger
+    debugger // eslint-disable-line no-debugger
   }
 }
